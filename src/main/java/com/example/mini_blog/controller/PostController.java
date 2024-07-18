2cd8913d763e038fe5022d166fa0a210a973de7a
yerm <leeyelim98@gmail.com>
2024-07-18 10:38:16 +0900

    CrossOrigin 추가

--- src/main/java/com/example/mini_blog/controller/PostController.java
+++ src/main/java/com/example/mini_blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.example.mini_blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.example.mini_blog.entity.PostEntity;
 import com.example.mini_blog.repository.PostRepository;
 
 @RestController
+@CrossOrigin
 public class PostController {
     @Autowired
     PostRepository postRepository;
