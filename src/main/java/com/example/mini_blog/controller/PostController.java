09ffcc3126be83696bb1245357d0a6cb4205df07
yerm <leeyelim98@gmail.com>
2024-07-18 10:30:42 +0900

    post-write

---
+++ src/main/java/com/example/mini_blog/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.example.mini_blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mini_blog.entity.PostEntity;
+import com.example.mini_blog.repository.PostRepository;
+
+@RestController
+public class PostController {
+    @Autowired
+    PostRepository postRepository;
+
+    // 입력을 받기 위해서 PostMapping 사용
+    // json 연습하기 위해서 RequestBody 사용
+    @PostMapping("/post-write")
+    public PostEntity postWrite(@RequestBody PostEntity post) {
+        PostEntity result = postRepository.save(post);
+        return result;
+    }
+}
