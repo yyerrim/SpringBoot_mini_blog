2ac7fb064b645a6990a77654330e6f65f3bd92c5
yerm <leeyelim98@gmail.com>
2024-07-18 13:18:50 +0900

    comment-write

--- src/main/java/com/example/mini_blog/controller/CommentController.java
+++ src/main/java/com/example/mini_blog/controller/CommentController.java
@@ -1,15 +1,31 @@
 package com.example.mini_blog.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mini_blog.entity.CommentEntity;
+import com.example.mini_blog.repository.CommentRepository;
 
 @RestController
 @CrossOrigin
 public class CommentController {
-    @GetMapping("/comment/{commentId}")
-    public void comment(@PathVariable Long commentId) {
+    @Autowired
+    CommentRepository commentRepository;
+
+    @PostMapping("/comment-write")
+    public Map<String, Object> commentWrite(@RequestBody CommentEntity comment) {
+        CommentEntity result = commentRepository.save(comment);
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("msg", "입력 완료");
+        map.put("result", result);
+        return map;
     }
 
 }
