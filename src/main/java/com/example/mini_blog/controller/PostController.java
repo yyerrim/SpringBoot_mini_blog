185fe9ac4f7d2a9f4a5f85af53b7682f09d4aa06
yerm <leeyelim98@gmail.com>
2024-07-18 10:45:35 +0900

    post-write 기능 추가 (Map - 개발자도구 network에서 확인 가능)

--- src/main/java/com/example/mini_blog/controller/PostController.java
+++ src/main/java/com/example/mini_blog/controller/PostController.java
@@ -1,5 +1,8 @@
 package com.example.mini_blog.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +20,19 @@ public class PostController {
 
     // 입력을 받기 위해서 PostMapping 사용
     // json 연습하기 위해서 RequestBody 사용
+    // @PostMapping("/post-write")
+    // public PostEntity postWrite(@RequestBody PostEntity post) {
+    // PostEntity result = postRepository.save(post);
+    // return result;
+    // }
     @PostMapping("/post-write")
-    public PostEntity postWrite(@RequestBody PostEntity post) {
+    public Map<String, Object> postWrite(@RequestBody PostEntity post) {
         PostEntity result = postRepository.save(post);
-        return result;
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        // 보통 성공의 의미로 200 사용
+        map.put("msg", "입력 완료");
+        map.put("result", result);
+        return map;
     }
 }
