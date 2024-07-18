4e0f6601058a7e64f76f24172327c3bdd5403ad3
yerm <leeyelim98@gmail.com>
2024-07-18 14:11:56 +0900

    post, comment 연관관계

--- src/main/java/com/example/mini_blog/entity/CommentEntity.java
+++ src/main/java/com/example/mini_blog/entity/CommentEntity.java
@@ -1,9 +1,12 @@
 package com.example.mini_blog.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity(name = "comment")
@@ -14,4 +17,8 @@ public class CommentEntity {
     Long id;
 
     String content;
+
+    @JsonIgnore // 무한반복 끊어주기(완벽한 해결방법은 아님)
+    @ManyToOne
+    PostEntity post;
 }
--- src/main/java/com/example/mini_blog/entity/PostEntity.java
+++ src/main/java/com/example/mini_blog/entity/PostEntity.java
@@ -1,9 +1,13 @@
 package com.example.mini_blog.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity(name = "post")
@@ -18,4 +22,7 @@ public class PostEntity {
     String desc;
     // description : DB는 DESC 명령어가 있어서 이렇게 작성하면 에러 발생
     // SpringBoot PDF 4.33 ===> application.properties에 추가
+
+    @OneToMany(mappedBy = "post")
+    List<CommentEntity> comments = new ArrayList<>();
 }
