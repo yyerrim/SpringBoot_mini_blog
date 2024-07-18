a76e56a74edb0bc4f34ddce4972f3890878e13d3
yerm <leeyelim98@gmail.com>
2024-07-18 09:42:39 +0900

    20240718 기본

---
+++ src/main/java/com/example/mini_blog/MiniBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_blog/MiniBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
